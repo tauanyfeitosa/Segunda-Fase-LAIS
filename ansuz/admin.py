9a8aec7723a74eab9e352c24156429e20689443e
Maria Tauany Santos Feitosa <tauanysanttos13@gmail.com>
2022-08-15 14:05:54 -0300

    add: registrado a model de PlanoCurso exibindo as informações pedidas no edital

--- ansuz/admin.py
+++ ansuz/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
-from ansuz.models import Usuario
+from ansuz.models import Usuario, PlanoCurso
 
 
 @admin.register(Usuario)
 class UsuarioAdmin(admin.ModelAdmin):
     list_display = ['nome_completo', 'cpf', 'data_de_nascimento', 'titulacao',]
     list_filter = ['titulacao',]
+
+@admin.register(PlanoCurso)
+class PlanoCursoAdmin(admin.ModelAdmin):
+    list_display = ['titulo','carga_horaria','area','prof_responsavel','status',]
+    list_filter = ['carga_horaria','area','status',]
