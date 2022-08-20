b64669b31a69f4940bf9d86757a2f74cef0dbdf4
Maria Tauany Santos Feitosa <tauanysanttos13@gmail.com>
2022-08-20 12:43:11 -0300

    fix: id's dos planos de curso são passados corretamente para os tópicos de aula

--- ansuz/urls.py
+++ ansuz/urls.py
@@ -3,11 +3,11 @@ from ansuz import views
 
 urlpatterns = [
 
-    path('', views.pginicial, name='pagina inicial'),
+    path('', views.pginicial, name='pagina_inicial'),
     path('cadastro', views.cadastro, name='cadastro'),
     path('login', views.login, name='login'),
     path('home', views.home, name='home'),
-    path('logout', views.logout_user, name = 'logout'),
-    path('autenticar', views.autenticar, name= 'autenticar'),
-    path('topicoscurso', views.topicoscurso, name = 'topicoscurso'),
+    path('logout', views.logout_user, name='logout'),
+    path('autenticar', views.autenticar, name='autenticar'),
+    path('topicoscurso/<int:id_plano>', views.topicoscurso, name='topicoscurso'),
 ]
