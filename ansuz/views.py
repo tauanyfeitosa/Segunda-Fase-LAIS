2553b5b3125cc61c7b2a11e6d28ecbb9443a8c5f
Maria Tauany Santos Feitosa <tauanysanttos13@gmail.com>
2022-08-21 00:45:38 -0300

    add: adição dos imports para a geração do pdf, função para detalhar topico, função para gerar certificado (assim como o codigo base para a formatação do pdf, função autenticar

--- ansuz/views.py
+++ ansuz/views.py
@@ -1,10 +1,15 @@
+import random
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import A4
 from django.contrib import messages, auth
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from django.db import transaction
 from django.shortcuts import redirect, render
-from ansuz.models import Usuario, PlanoCurso, TopicoAula
-from ansuz.forms import CadastroForm, LoginForms, SubmeterNovoPlanoForm, CadastrarTopicoAulaForm
+
+from ansuz import models
+from ansuz.models import Usuario, PlanoCurso, TopicoAula, Certificado, Verificador
+from ansuz.forms import CadastroForm, LoginForms, SubmeterNovoPlanoForm, CadastrarTopicoAulaForm, VerificadorForm
 
 
 def cadastro(request):
@@ -57,9 +62,6 @@ def home(request):
                                  f'Seu Plano de Curso {form.cleaned_data.get("titulo")} foi cadastrado com sucesso!')
                 return redirect('home')
     planos = request.user.planos_curso.all()
-    for plano in planos:
-        status = plano.get_status_display()
-        id_plano=plano.pk
     return render(request, 'usuarios/home.html', locals())
 
 @login_required()
@@ -82,3 +84,70 @@ def topicoscurso(request, id_plano):
                 return redirect('home')
     return render(request, 'usuarios/topicoscurso.html', locals())
 
+@login_required
+def detalhar_topico(request, id_topico):
+    topicos = request.user.planos_curso.all()
+    for topico in topicos:
+        topico_detalhado = TopicoAula.objects.get(pk=id_topico)
+        topico_descricao = topico_detalhado.text_rendered
+
+    return render(request, 'usuarios/detalhar_topico.html', locals())
+
+def certificado(request,id_plano):
+    def generate_codigo(n):
+        base = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        codigo = ""
+        while (n):
+            codigo += base[random.randint(1, 1000) % 62]
+            n -= 1
+        return codigo
+    codigo_verificador = generate_codigo(15)
+    certificado_pdf = Certificado.objects.create(titulo=PlanoCurso.objects.filter(pk=id_plano),
+                                                 prof_responsável=request.user,
+                                                 area_tematica=PlanoCurso.objects.filter(pk=id_plano),
+                                                 carga_horaria=PlanoCurso.objects.filter(pk=id_plano),
+                                                 ementa=PlanoCurso.objects.filter(pk=id_plano),
+                                                 obj_geral=PlanoCurso.objects.filter(pk=id_plano),
+                                                 avaliacao=PlanoCurso.objects.filter(pk=id_plano),
+                                                 data=PlanoCurso.objects.filter(pk=id_plano),
+                                                 codigo_verificador=models.cleaned_data.get(),
+                                                 criado_em=models.cleaned_data.get()
+                                                 )
+    def mm2p(milimetros):
+        return milimetros / 0.352777
+
+    try:
+        x = '<INSERIR VARIÁVEL>'
+        cnv = canvas.Canvas('certificado.pdf', pagesize=A4)
+        cnv.drawImage('imagens/lais.jpg', mm2p(100), mm2p(20), width=mm2p(15), height=mm2p(15))
+        cnv.drawImage('imagens/assinatura.jpeg', mm2p(70), mm2p(54), width=mm2p(75), height=mm2p(25))
+        cnv.drawImage('imagens/design.png', mm2p(0), mm2p(222), width=mm2p(210), height=mm2p(75))
+        cnv.drawString(mm2p(40), mm2p(249), "Dados: " + x) #nome e cpf do professor
+        cnv.drawString(mm2p(78), mm2p(230), 'CERTIFICADO DE APROVAÇÃO')
+        cnv.drawString(mm2p(30), mm2p(190), "Certificamos que, para os devidos fins, o(a) professor(a)")
+        cnv.drawString(mm2p(30), mm2p(180), x) #nome do professor
+        cnv.drawString(mm2p(30), mm2p(170), "obteve aprovação da Plataforma Ansuz - AVASUS para a")
+        cnv.drawString(mm2p(30), mm2p(160), "ministração do curso " + x) #titulo do curso
+        cnv.drawString(mm2p(30), mm2p(150), "sob o qual consta as seguintes informações:")
+        cnv.drawString(mm2p(30), mm2p(140), "Título: " + x) #titulo do curso
+        cnv.drawString(mm2p(30), mm2p(130), "Área Temática: " + x) #area tematica
+        cnv.drawString(mm2p(30), mm2p(120), "Carga Horária: " + x) #carga horaria
+        cnv.drawString(mm2p(30), mm2p(110), "Data de Submissão: " + x) #data de criação do curso
+        cnv.drawString(mm2p(30), mm2p(85), 'Data de Emissão: ' + x) #data de emissão do certificado
+        cnv.drawString(mm2p(78), mm2p(16), 'Comunidade ANSUZ - AVASUS')
+        cnv.drawString(mm2p(78), mm2p(54), 'Maria Tauany Santos Feitosa')
+        cnv.drawString(mm2p(82), mm2p(48), 'Diretoria-Geral ANSUZ')
+        cnv.drawString(mm2p(10), mm2p(5), 'Código de Autenticidade: ')
+        cnv.save()
+    except:
+        print('pdf invalido')
+
+
+def autenticar(request):
+    form = VerificadorForm(request.POST or None)
+    if request.method == 'POST':
+        form = VerificadorForm(request.POST)
+        if form.is_valid():
+            codigo = Verificador.objects.create(codigo=form.cleaned_data.get('codigo'))
+            return redirect('autenticar')
+    return render(request, 'usuarios/autenticar.html', locals())
