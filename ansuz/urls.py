5d83764d57e7eec46a93f84d52c773021cb6bf4f
Maria Tauany Santos Feitosa <tauanysanttos13@gmail.com>
2022-08-21 00:42:52 -0300

    add: adição da path detalhar topico

--- ansuz/urls.py
+++ ansuz/urls.py
@@ -3,11 +3,12 @@ from ansuz import views
 
 urlpatterns = [
 
-    path('', views.pginicial, name='pagina_inicial'),
+    path('', views.pginicial, name='pginicial'),
     path('cadastro', views.cadastro, name='cadastro'),
     path('login', views.login, name='login'),
     path('home', views.home, name='home'),
     path('logout', views.logout_user, name='logout'),
     path('autenticar', views.autenticar, name='autenticar'),
     path('topicoscurso/<int:id_plano>', views.topicoscurso, name='topicoscurso'),
+    path('detalhar_topico/<int:id_topico>', views.detalhar_topico, name='detalhar_topico')
 ]
